82f411e3fc3516f1c06c29082a0d32dc00417f2f
pcannon09 <pcannon775@gmail.com>
2026-02-16 17:10:58 +0100

    Do not use pragma header guard

--- c/EXAMPLEpredefines.h
+++ c/EXAMPLEpredefines.h
@@ -4,15 +4,16 @@
  * STD Information: 20250723 - 1.0S
  */
 
-#pragma once
+#ifndef INCLUDE_C_EXAMPLEPREDEFINES_H_
+#define INCLUDE_C_EXAMPLEPREDEFINES_H_
 
 // Project setup
-#define EXAMPLE_DEFAULT_CPP_STD			201103L
+#define EXAMPLE_DEFAULT_C_STD			201112L
 
 // Versioning
 #define EXAMPLE_VERSION_MAJOR            0
 #define EXAMPLE_VERSION_MINOR            0
-#define EXAMPLE_VERSION_PATCH            0
+#define EXAMPLE_VERSION_PATCH            1
 
 #define EXAMPLE_VERSION_STD              0
 
@@ -22,10 +23,10 @@
 // * build
 #define EXAMPLE_VERSION_STATE          "dev"
 
-#define EXAMPLE_VERSION                ((EXAMPLE_VERSION_MAJOR<<16)|(FDX_VERSION_MINOR<<8)|(FDX_VERSION_PATCH)|(FDX_VERSION_STATE << 24))
+#define EXAMPLE_VERSION                ((EXAMPLE_VERSION_MAJOR<<16)|(EXAMPLE_VERSION_MINOR<<8)|(EXAMPLE_VERSION_PATCH)|(EXAMPLE_VERSION_STATE << 24))
 
-#define EXAMPLE_VERSION_CHECK(EXAMPLE_VERSION_MAJOR, FDX_VERSION_MINOR, FDX_VERSION_PATCH, FDX_VERSION_STATE) \
-    (((EXAMPLE_VERSION_MAJOR)<<16)|((EXAMPLE_VERSION_MINOR)<<8)|(FDX_VERSION_PATCH)|((FDX_VERSION_STATE) << 24))
+#define EXAMPLE_VERSION_CHECK(EXAMPLE_VERSION_MAJOR, EXAMPLE_VERSION_MINOR, EXAMPLE_VERSION_PATCH, EXAMPLE_VERSION_STATE) \
+    (((EXAMPLE_VERSION_MAJOR)<<16)|((EXAMPLE_VERSION_MINOR)<<8)|(EXAMPLE_VERSION_PATCH)|((EXAMPLE_VERSION_STATE) << 24))
 
 // Macro utils
 #define EXAMPLE_STRINGIFY(x) #x
@@ -35,15 +36,19 @@
 #   define EXAMPLE_DEV true
 #endif
 
-#ifdef WIN32
+#if defined(WIN32) || defined(_WIN32)
 #	define EXAMPLE_OS_WIN32
 #elif defined(__APPLE__) || defined(__MACH__) || defined(Macintosh)
 #	define EXAMPLE_OS_MACOS
-#elif defined(__linux__) || defined(__unix) || defined(__unix__)
+#elif defined(__linux__)
 #	define EXAMPLE_OS_UNIX_LINUX
+#elif defined(__unix) || defined(__unix__)
+#	define EXAMPLE_OS_UNIX
 #elif defined(__FreeBSD__)
 #	define EXAMPLE_OS_FREEBSD
 #else
-#	error "Current platform is not supported"
+#	error "Current platform might not supported"
 #endif // defined(WIN32) // Platform check
+
+#endif  // INCLUDE_C_EXAMPLEPREDEFINES_H_
 
